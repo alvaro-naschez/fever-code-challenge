fa5a7e6863eb098374c2c0a40fffe99e39d5b41c
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2022-12-20 19:10:03 +0100

    refactor: remove a couple of comments

--- fever/models/event.py
+++ fever/models/event.py
@@ -2,13 +2,10 @@ from sqlalchemy import Column, DateTime, Integer, Text
 
 from fever.db.base_class import Base
 
-# from sqlalchemy.dialects.postgresql import UUID
-
 
 class Event(Base):
     __tablename__ = "events"
 
-    # id = Column(UUID, primary_key=True)
     id = Column(Integer, primary_key=True)
     title = Column(Text, nullable=False)
     start_date = Column(DateTime, nullable=False)
