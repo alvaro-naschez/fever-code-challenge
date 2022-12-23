9674fff850ff4169cdb8258dd5994cd1c04b4041
Alvaro Sanchez <alvarosanchezpalomino@gmail.com>
2022-12-23 10:18:03 +0100

    refactor: imrove test_read_events readability

--- fever/models/event.py
+++ fever/models/event.py
@@ -1,14 +1,18 @@
+from dataclasses import dataclass
+from datetime import datetime
+
 from sqlalchemy import Column, DateTime, Integer, Text
 
 from fever.db.base_class import Base
 
 
+@dataclass
 class Event(Base):
     __tablename__ = "events"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(Text, nullable=False)
-    start_date = Column(DateTime, nullable=False)
-    end_date = Column(DateTime)
-    min_price = Column(Integer)
-    max_price = Column(Integer)
+    id: int = Column(Integer, primary_key=True)
+    title: str = Column(Text, nullable=False)
+    start_date: datetime = Column(DateTime, nullable=False)
+    end_date: datetime = Column(DateTime)
+    min_price: datetime = Column(Integer)
+    max_price: datetime = Column(Integer)
